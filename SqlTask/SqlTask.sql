054c573829d4f89f8d1edbb2bf27b9f90258fb0b
Илья <williamvinogradov@outlook.com>
2018-02-11 22:54:16 +0300

    Add sql task + update gitignore

---
+++ SqlTask/SqlTask.sql
@@ -0,0 +1,11 @@
+﻿select distinct Name from buys as b
+left join clients as c
+on c.CustomerId = b.CustomerId
+where ProductName = 'Молоко'
+and b.CustomerId not in 
+(
+  select CustomerId from buys
+  where PurchaiseDatetime > DATEADD(MONTH, -1, GETDATE())
+  group by CustomerId, ProductName
+  having ProductName = 'Сметана'
+)
